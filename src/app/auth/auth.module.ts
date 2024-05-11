bff992cae3eeab53b8e80cd5a310ab2a291bb13f
U2D-8Bits <u2d.8bits@gmail.com>
2024-05-11 18:17:53 -0500

    routes update

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { RouterModule } from '@angular/router';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPageComponent,
+    AuthLayoutComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    RouterModule
+  ],
+  exports: [
+    AuthLayoutComponent,
+    LoginPageComponent
   ]
 })
 export class AuthModule { }
