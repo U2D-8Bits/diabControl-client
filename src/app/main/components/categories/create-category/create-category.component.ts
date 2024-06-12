201e21f48c24ed269ce44268e5a4ea2fed787e3e
U2D-8Bits <u2d.8bits@gmail.com>
2024-06-11 19:16:48 -0500

    Previous Chat

--- src/app/main/components/categories/create-category/create-category.component.ts
+++ src/app/main/components/categories/create-category/create-category.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CategoryService } from '../../../services/categories.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'main-create-category',
   templateUrl: './create-category.component.html',
   styleUrl: './create-category.component.css'
 })
-export class CreateCategoryComponent {
+export class CreateCategoryComponent implements OnInit{
+
+  //? Variables e Inyecciones
+  private categoryService = inject( CategoryService )
+  private fb = inject( FormBuilder )
+
+  categoryForm = this.fb.group({
+    name_category: ['', [Validators.required] ],
+  })
+
+  ngOnInit(){
+    console.log(`CreateCategoryComponent initialized!`)
+    
+  }
+
+  //? Metodo para crear una categoria
+  createCategory(){
+    const categoryData = this.categoryForm.value
+  }
+
+  ngOnDestroy(){
+    console.log(`CreateCategoryComponent destroyed!`)
+  }
 
 }
--- src/app/main/components/categories/list-categories/list-categories.component.ts
+++ src/app/main/components/categories/list-categories/list-categories.component.ts
@@ -1,18 +1,111 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Category } from '../../../interfaces/categories/category.interface';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
+import { CategoryService } from '../../../services/categories.service';
+import { CreateCategoryComponent } from '../create-category/create-category.component';
+import { ViewCategoryComponent } from '../view-category/view-category.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'main-list-categories',
   templateUrl: './list-categories.component.html',
-  styleUrl: './list-categories.component.css'
+  styleUrl: './list-categories.component.css',
+  providers: [ConfirmationService, MessageService, DialogService]
 })
 export class ListCategoriesComponent implements OnInit{
 
-  //Variables e Inyecciones
+  //? Variables e Inyecciones
+  private confirmationService = inject( ConfirmationService)
+  private messageService = inject( MessageService)
+  public dialogService = inject( DialogService )
+  private categoryService = inject( CategoryService )
+  public categories: Category[] = [];
 
   ngOnInit(): void {
     console.log(`ListCategoriesComponent initialized!`)
+
+    this.getAllCategories();
   }
 
+
+  showDialog(componentName: string, headerText: string){
+
+    if( componentName === 'create'){
+      this.dialogService.open(CreateCategoryComponent,{
+        header: headerText,
+        breakpoints: { '960px': '500px', '640px': '100vw' },
+        style: { 'max-width': '90vw', width: '80vw' },
+        height: '80%',
+        contentStyle: { overflow: 'auto' },
+      })
+    }
+
+    if( componentName === 'view'){
+      this.dialogService.open(ViewCategoryComponent, {
+        header: headerText,
+        breakpoints: { '960px': '500px', '640px': '100vw' },
+        style: { 'max-width': '90vw', width: '80vw' },
+        height: '80%',
+        contentStyle: { overflow: 'auto' },
+      })
+    }
+
+  }
+
+
+
+  //? Metodo para obtener todas las categorias
+  getAllCategories(){
+    this.categoryService.getAllCategories()
+    .subscribe({
+      next: (categories) => {
+        this.categories = categories
+      },
+      error: (error) => {
+        console.error(error)
+      }
+    })
+  }
+
+
+
+  //? Metodo para eliminar una categoria
+  deleteCategory(id: number){
+
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: "¡No podrás revertir esto!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '¡Sí, bórralo!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.categoryService.deleteCategory(id)
+        .subscribe({
+          next: (resp) => {
+            this.getAllCategories();
+            Swal.fire(
+              '¡Eliminado!',
+              'La categoría ha sido eliminada.',
+              'success'
+            )
+          },
+          error: (error) => {
+            console.error(error)
+            Swal.fire(
+              'Error!',
+              'Ha ocurrido un error al intentar eliminar la categoría.',
+              'error'
+            )
+          }
+        })
+      }
+    })
+
+  }
 
   ngOnDestroy(): void {
     console.log(`ListCategoriesComponent destroyed!`)
