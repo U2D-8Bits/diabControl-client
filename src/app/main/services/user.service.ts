e7c0ab5419b6397219507f5ddd8d3f5eea9b1a93
U2D-8Bits <u2d.8bits@gmail.com>
2024-08-04 16:21:31 -0500

    List Patients & Medics with search Service

--- src/app/main/services/user.service.ts
+++ src/app/main/services/user.service.ts
@@ -5,54 +5,26 @@ import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
 import { User } from '../../auth/interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
-  constructor() { }
+  constructor() {}
 
   //? Variables e Inyecciones
   private readonly baseUrl: string = environment.baseUrl;
-  private http = inject( HttpClient);
+  private http = inject(HttpClient);
 
   //? Funciones
 
-
-
-
-
   //? Obtener todos los usuarios
-  getAllUsers(): Observable<User[]>{
+  getAllUsers(): Observable<User[]> {
     const url = `${this.baseUrl}/users`;
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    
-     return this.http.get<User[]>(url, {headers})
-     .pipe(
-       map((users: User[]) => {
-         return users 
-       }),
-       catchError((err: any) => {
-         return throwError(err);
-       })
-     );
-  }
 
-
-
-
-
-  //? Obtener todos los usuarios de rol paciente
-  getAllPatients(): Observable<User[]>{
-
-    const url = `${this.baseUrl}/users/pacientes`;
-    const token = localStorage.getItem('token');
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-
-    return this.http.get<User[]>(url, {headers})
-    .pipe(
+    return this.http.get<User[]>(url, { headers }).pipe(
       map((users: User[]) => {
-        return users
+        return users;
       }),
       catchError((err: any) => {
         return throwError(err);
@@ -60,24 +32,40 @@ export class UserService {
     );
   }
 
+  //? Obtener todos los usuarios de rol paciente
+  getAllPatients(): Observable<User[]> {
+    const url = `${this.baseUrl}/users/pacientes`;
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<User[]>(url, { headers }).pipe(
+      map((users: User[]) => {
+        return users;
+      }),
+      catchError((err: any) => {
+        return throwError(err);
+      })
+    );
+  }
 
   //? Obtener todos los usuarios de rol pacientes paginados
-  getAllPatientsPaginated(page: number, limit: number, search: string): Observable<any>{
+  getAllPatientsPaginated(
+    page: number,
+    limit: number,
+    search: string
+  ): Observable<any> {
     const url = `${this.baseUrl}/users/pacientes/paginated`;
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    let params = new HttpParams()
-      .set('page', page)
-      .set('limit', limit)
-      if(search){
-        params = params.set('search', search)
-      }
+    let params = new HttpParams().set('page', page).set('limit', limit);
+    if (search) {
+      params = params.set('search', search);
+    }
 
-    return this.http.get<any>(url, {headers, params})
-    .pipe(
+    return this.http.get<any>(url, { headers, params }).pipe(
       map((resp: any) => {
-        return resp
+        return resp;
       }),
       catchError((err: any) => {
         return of(err);
@@ -86,22 +74,23 @@ export class UserService {
   }
 
   //? Obtener todos los usuarios de rol medico paginados
-  getAllMedicsPaginated(page: number, limit: number, search: string): Observable<any>{
+  getAllMedicsPaginated(
+    page: number,
+    limit: number,
+    search: string
+  ): Observable<any> {
     const url = `${this.baseUrl}/users/medics/paginated`;
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    let params = new HttpParams()
-      .set('page', page)
-      .set('limit', limit)
-      if(search){
-        params = params.set('search', search)
-      }
+    let params = new HttpParams().set('page', page).set('limit', limit);
+    if (search) {
+      params = params.set('search', search);
+    }
 
-    return this.http.get<any>(url, {headers, params})
-    .pipe(
+    return this.http.get<any>(url, { headers, params }).pipe(
       map((resp: any) => {
-        return resp
+        return resp;
       }),
       catchError((err: any) => {
         return of(err);
@@ -109,60 +98,47 @@ export class UserService {
     );
   }
 
-
-
-
   //? Obtener todos los usuarios de rol medico
-  getAllMedics(): Observable<User[]>{
+  getAllMedics(): Observable<User[]> {
     const url = `${this.baseUrl}/users/medics`;
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    return this.http.get<User[]>(url, {headers})
-    .pipe(
+    return this.http.get<User[]>(url, { headers }).pipe(
       map((users: User[]) => {
-        return users
+        return users;
       }),
       catchError((err: any) => {
         return throwError(err);
       })
     );
   }
-
-
-
 
   //? Obtener un usuario por ID
-  getUserById(id: number): Observable<User>{
+  getUserById(id: number): Observable<User> {
     const url = `${this.baseUrl}/users/${id}`;
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    return this.http.get<User>(url, {headers})
-    .pipe(
+    return this.http.get<User>(url, { headers }).pipe(
       map((user: User) => {
-        return user
+        return user;
       }),
       catchError((err: any) => {
         return throwError(err);
       })
     );
   }
-
-
-
-
 
   //? Crear un usuario
-  createUser(userData: any): Observable<User>{
-    const url= `${this.baseUrl}/users`;
+  createUser(userData: any): Observable<User> {
+    const url = `${this.baseUrl}/users`;
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    return this.http.post<User>(url, userData, {headers})
-    .pipe(
+    return this.http.post<User>(url, userData, { headers }).pipe(
       map((user: User) => {
-        return user
+        return user;
       }),
       catchError((err: any) => {
         return throwError(err);
@@ -170,37 +146,29 @@ export class UserService {
     );
   }
 
-
-
-
-
   //? Actualizar un usuario por ID
-  updateUser(id: number, userData: any): Observable<User>{
+  updateUser(id: number, userData: any): Observable<User> {
     const url = `${this.baseUrl}/users/${id}`;
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    return this.http.patch<User>(url, userData, {headers})
-    .pipe(
+    return this.http.patch<User>(url, userData, { headers }).pipe(
       map((user: User) => {
-        return user
+        return user;
       }),
       catchError((err: any) => {
         return throwError(err);
       })
     );
   }
-
-
 
   //? Actualizar perfil de usuario
-  updateProfile(id: number, profileData: any): Observable<User>{
+  updateProfile(id: number, profileData: any): Observable<User> {
     const url = `${this.baseUrl}/users/${id}/profile`;
 
-    return this.http.patch<User>(url, profileData)
-    .pipe(
+    return this.http.patch<User>(url, profileData).pipe(
       map((user: User) => {
-        return user
+        return user;
       }),
       catchError((err: any) => {
         return throwError(err);
@@ -208,20 +176,15 @@ export class UserService {
     );
   }
 
-
-
-
-
   //? Cambiar el estado del usuario
-  changeUserState(id: number): Observable<boolean>{
+  changeUserState(id: number): Observable<boolean> {
     const url = `${this.baseUrl}/users/${id}/status`;
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    return this.http.patch<User>(url, {}, {headers})
-    .pipe(
+    return this.http.patch<User>(url, {}, { headers }).pipe(
       map((resp: User) => {
-        return resp.user_status
+        return resp.user_status;
       }),
       catchError((err: any) => {
         return throwError(err);
@@ -229,20 +192,15 @@ export class UserService {
     );
   }
 
-
-
-
-
   //? Eliminar un usuario
-  deleteUser(id: number): Observable<boolean>{
+  deleteUser(id: number): Observable<boolean> {
     const url = `${this.baseUrl}/users/${id}`;
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    return this.http.delete<User>(url, {headers})
-    .pipe(
+    return this.http.delete<User>(url, { headers }).pipe(
       map((resp: any) => {
-        return true
+        return true;
       }),
       catchError((err: any) => {
         return throwError(err);
@@ -250,19 +208,63 @@ export class UserService {
     );
   }
 
-    //? Obtener el usuario actual
-    getCurrentUser(): Observable<User> {
-      const token = localStorage.getItem('token');
-      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-  
-      return this.http.get<User>(`${this.baseUrl}/auth/me`, { headers })
-        .pipe(
-          map((user: User) => {
-            return user;
-          }),
-          catchError((err: any) => {
-            return throwError(err);
-          })
-        );
+  //? Obtener el usuario actual
+  getCurrentUser(): Observable<User> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<User>(`${this.baseUrl}/auth/me`, { headers }).pipe(
+      map((user: User) => {
+        return user;
+      }),
+      catchError((err: any) => {
+        return throwError(err);
+      })
+    );
+  }
+
+
+  //? Obtener los usuarios de rol paciente con busqueda
+  getPatientsWithSearch(search: string = ''): Observable<User[]>{
+    let params = new HttpParams();
+    const url = `${this.baseUrl}/users/pacientes/search`;
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    if(search){
+      params = params.append('search', search);
     }
+
+    return this.http.get<User[]>(url, { headers, params })
+    .pipe(
+      map((users: User[]) => {
+        return users;
+      }),
+      catchError((err: any) => {
+        return throwError(err);
+      })
+    );
+  }
+
+  //? Obtener los usuarios de rol medico con busqueda
+  getMedicsWithSearch(search: string = ''): Observable<User[]>{
+    let params = new HttpParams();
+    const url = `${this.baseUrl}/users/medicos/search`;
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    if(search){
+      params = params.append('search', search);
+    }
+
+    return this.http.get<User[]>(url, { headers, params })
+    .pipe(
+      map((users: User[]) => {
+        return users;
+      }),
+      catchError((err: any) => {
+        return throwError(err);
+      })
+    );
+  }
 }
